419c258fe8810dc5cee4c3f9ece5469cb3706268
Vladimir Aleksandrov <invis87@gmail.com>
2014-02-10 17:05:46 +0400

    Create custom constructors for Event

--- src/main/java/com/appmetr/s2s/Event.java
+++ src/main/java/com/appmetr/s2s/Event.java
@@ -1,16 +1,26 @@
 package com.appmetr.s2s;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Event {
 
     private String event;
-    private long timeStamp;
+    private long timestamp;
     private Map<String, Object> properties;
 
-    public Event(String event, long timeStamp, Map<String, Object> properties) {
+    public Event(String event){
+        this(event, new Date().getTime(), new HashMap<String, Object>());
+    }
+
+    public Event(String event, long timestamp){
+        this(event, timestamp, new HashMap<String, Object>());
+    }
+
+    public Event(String event, long timestamp, Map<String, Object> properties) {
         this.event = event;
-        this.timeStamp = timeStamp;
+        this.timestamp = timestamp;
         this.properties = properties;
     }
 
@@ -22,13 +32,13 @@ public class Event {
         return properties;
     }
 
-    public long getTimeStamp() {
-        return timeStamp;
+    public long getTimestamp() {
+        return timestamp;
     }
 
     public int calcApproximateSize() {
         int propertiesSize = 40 + (40 * properties.size()); //40 - Map size and 40 - each entry overhead
-        propertiesSize += String.valueOf(timeStamp).length() * 2 + 24 + 16;
+        propertiesSize += String.valueOf(timestamp).length() * 2 + 24 + 16;
 
         for (Map.Entry<String, Object> entry : properties.entrySet()) {
             propertiesSize += entry.getKey().length() * 2 + 24 + 16;    //24 - String object size, 16 - char[]

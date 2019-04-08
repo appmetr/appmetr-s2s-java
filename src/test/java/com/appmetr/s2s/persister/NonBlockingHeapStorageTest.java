555f660ac46bf0a2c96a299f1a7bb4b3ad140d64
Tomas Shestakov <t.shestakov@pixonic.com>
2019-04-08 15:36:13 +0300

    PIX-2411 using single dependency for JUnit 5

---
+++ src/test/java/com/appmetr/s2s/persister/NonBlockingHeapStorageTest.java
@@ -0,0 +1,12 @@
+package com.appmetr.s2s.persister;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NonBlockingHeapStorageTest {
+
+    @Test
+    void store() {
+    }
+}

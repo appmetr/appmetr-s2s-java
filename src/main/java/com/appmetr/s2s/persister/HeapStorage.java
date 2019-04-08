df7e44cf29f1faf363c6b2a9ddee13ac75834210
Tomas Shestakov <t.shestakov@pixonic.com>
2019-04-08 11:26:01 +0300

    PIX-2411 do not override store method in HeapStorage

--- src/main/java/com/appmetr/s2s/persister/HeapStorage.java
+++ src/main/java/com/appmetr/s2s/persister/HeapStorage.java
@@ -1,19 +1,11 @@
 package com.appmetr.s2s.persister;
 
 import com.appmetr.s2s.BinaryBatch;
-import com.appmetr.s2s.events.Action;
 
-import java.util.Collection;
-
+/**
+ * Method store() is always successful and always returns true but can block until space become available
+ */
 public class HeapStorage extends AbstractHeapStorage {
-
-    /**
-     * Store operation is always successful but can block until space become available
-     * @return always true
-     */
-    @Override public synchronized boolean store(Collection<Action> actions, BatchFactory batchFactory) throws InterruptedException {
-        return super.store(actions, batchFactory);
-    }
 
     @Override protected boolean isCapacityExceeded(BinaryBatch binaryBatch) throws InterruptedException {
         while (true) {

3c80c868b125082b788962b7a9f4630939697c8b
Tomas Shestakov <shestakovte@gmail.com>
2019-04-09 21:56:44 +0300

    PIX-2411 charset for Reader

--- src/main/java/com/appmetr/s2s/HttpRequestService.java
+++ src/main/java/com/appmetr/s2s/HttpRequestService.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.time.Clock;
 import java.util.HashMap;
 import java.util.Map;
@@ -129,7 +130,7 @@ public class HttpRequestService {
 
     protected String readResponse(InputStream inputStream) throws IOException {
         final StringBuilder result = new StringBuilder();
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             String inputLine;
             while ((inputLine = input.readLine()) != null) {
                 result.append(inputLine);

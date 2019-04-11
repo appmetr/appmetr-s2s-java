0d9e6e9a03b4b694132ef39d8af250e9fe3da0c6
Tomas Shestakov <t.shestakov@pixonic.com>
2019-04-11 11:16:08 +0300

    PIX-2411 fixed dependencies

---
+++ src/test/java/com/appmetr/s2s/sender/HttpBatchSenderIT.java
@@ -0,0 +1,20 @@
+package com.appmetr.s2s.sender;
+
+import org.junit.jupiter.api.Test;
+
+class HttpBatchSenderIT {
+
+    @Test
+    void send() {
+//        WireMockServer wireMockServer = new WireMockServer(0);
+//        wireMockServer.start();
+//
+//        configureFor(wireMockServer.port());
+//
+//        stubFor(get("/api").willReturn(aResponse().withStatus(200)));
+//
+//        verify(1, getRequestedFor(urlEqualTo("/api")));
+//
+//        wireMockServer.stop();
+    }
+}

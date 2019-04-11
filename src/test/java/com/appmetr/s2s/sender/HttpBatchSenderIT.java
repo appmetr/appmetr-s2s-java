d36e16128ab723bbf2550cac3bb9a9b2012ba817
Tomas Shestakov <t.shestakov@pixonic.com>
2019-04-11 11:31:54 +0300

    PIX-2411 failsafe plugin turned on

--- src/test/java/com/appmetr/s2s/sender/HttpBatchSenderIT.java
+++ src/test/java/com/appmetr/s2s/sender/HttpBatchSenderIT.java
@@ -1,20 +1,23 @@
 package com.appmetr.s2s.sender;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 class HttpBatchSenderIT {
 
     @Test
     void send() {
-//        WireMockServer wireMockServer = new WireMockServer(0);
-//        wireMockServer.start();
-//
-//        configureFor(wireMockServer.port());
-//
-//        stubFor(get("/api").willReturn(aResponse().withStatus(200)));
-//
-//        verify(1, getRequestedFor(urlEqualTo("/api")));
-//
-//        wireMockServer.stop();
+        WireMockServer wireMockServer = new WireMockServer(0);
+        wireMockServer.start();
+
+        configureFor(wireMockServer.port());
+
+        stubFor(get("/api").willReturn(aResponse().withStatus(200)));
+
+        verify(1, getRequestedFor(urlEqualTo("/api")));
+
+        wireMockServer.stop();
     }
 }

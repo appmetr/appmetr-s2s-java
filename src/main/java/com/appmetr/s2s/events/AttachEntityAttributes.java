24be649ae7807658f26d4e5785a914092ecfc21d
Tomas Shestakov <t.shestakov@pixonic.com>
2019-08-22 11:15:05 +0300

    added equals and hashCode methods

--- src/main/java/com/appmetr/s2s/events/AttachEntityAttributes.java
+++ src/main/java/com/appmetr/s2s/events/AttachEntityAttributes.java
@@ -1,5 +1,7 @@
 package com.appmetr.s2s.events;
 
+import java.util.Objects;
+
 public class AttachEntityAttributes extends Action {
     public static final String ACTION = "attachEntityAttributes";
 
@@ -10,6 +12,19 @@ public class AttachEntityAttributes extends Action {
         super(ACTION);
         this.entityName = entityName;
         this.entityValue = entityValue;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AttachEntityAttributes)) return false;
+        if (!super.equals(o)) return false;
+        AttachEntityAttributes that = (AttachEntityAttributes) o;
+        return Objects.equals(entityName, that.entityName) &&
+                Objects.equals(entityValue, that.entityValue);
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(super.hashCode(), entityName, entityValue);
     }
 
     @Override public String toString() {
